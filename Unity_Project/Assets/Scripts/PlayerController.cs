05b3d380ec531818a429135f616b892d0f37f824
2024 - PCSC - 11 - Nieland <419238@bethel.wednet.edu>
2024-09-19 08:32:22 -0700

    Initial Project Setup

---
+++ Unity_Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody myRb;
+    Camera playerCam;
+    Vector2 camRotation;
+
+    public int health = 5;
+    public int maxHealth = 10;
+    public int healthPickAmt = 5;
+
+    public Transform weaponSlot;
+
+    public bool sprinting = false;
+    public bool sprintToggle = false;
+    public float sprintmult = 1.5f;
+    public float speed = 10f;
+    public float jumphight = 5f;
+
+    public float mouseSensitivity = 2.0f;
+    public float Xsensitivity = 2.0f;
+    public float Ysensitivity = 2.0f;
+    public float grounddetection = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRb = GetComponent<Rigidbody>();
+        playerCam = gameObject.transform.GetChild(0).GetComponent<Camera>();
+        camRotation = Vector2.zero;
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        camRotation.x += Input.GetAxisRaw("Mouse X") * mouseSensitivity;
+        camRotation.y += Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+
+        camRotation.y = Mathf.Clamp(camRotation.y, -90, 90);
+
+        playerCam.transform.localRotation = Quaternion.AngleAxis(camRotation.y, Vector3.left);
+        transform.localRotation = Quaternion.AngleAxis(camRotation.x, Vector3.up);
+
+        if (!sprinting && !sprintToggle && Input.GetKey(KeyCode.LeftShift))
+            sprinting = true;
+
+        if (sprinting && sprintToggle && (Input.GetAxisRaw("Vertical") > 0) && Input.GetKey(KeyCode.LeftShift))
+            sprinting = true;
+
+        Vector3 temp = myRb.velocity;
+
+        temp.x = Input.GetAxisRaw("Horizontal") * speed;
+        temp.z = Input.GetAxisRaw("Vertical") * speed;
+        
+        if (sprinting)
+            temp.z *= sprintmult;
+
+        if (sprinting && !sprintToggle && Input.GetAxisRaw("Vertical") <= 0)
+             sprinting = false;
+
+        if (sprinting && !sprintToggle && Input.GetKeyUp(KeyCode.LeftShift))
+            sprinting = false;
+
+        if (Input.GetKeyDown(KeyCode.Space) && Physics.Raycast(transform.position, -transform.up, grounddetection))
+           temp.y = jumphight;
+       
+        myRb.velocity = (transform.forward * temp.z) + (transform.right * temp.x) + (transform.up * temp.y);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Pickup" && health < maxHealth)
+        {
+            if (health + healthPickAmt > maxHealth)
+                health = maxHealth;
+            else health += healthPickAmt;
+            Destroy(collision.gameObject);
+        }
+    }
+}
